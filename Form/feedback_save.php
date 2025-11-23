558b9cb827219f7feb008f9ad6c27f551bdb61aa
Chaiyapond Matemaolee - 6809617043 <chaiyapond.mat@dome.tu.ac.th>
2025-11-24 06:48:31 +0700

    Create feedback_save.php and update Feedback.html to link to feedback_save.php

---
+++ Form/feedback_save.php
@@ -0,0 +1,38 @@
+<?php
+
+$dataFile = 'feedback_data.json';
+
+if (file_exists($dataFile)) {
+    $records = json_decode(file_get_contents($dataFile), true) ?? [];
+} else {
+    $records = [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $name = trim($_POST['fullname_feedback'] ?? '');
+    $rating = trim($_POST['rating'] ?? '');
+    $favorite = trim($_POST['favorite_booth'] ?? '');
+    $comment = trim($_POST['comment'] ?? '');
+    $consent = isset($_POST['consent']) ? true : false;
+
+    if ($name && $rating && $favorite && $consent) {
+
+        $records[] =  [
+            'name' => $name,
+            'rating' => $rating,
+            'favorite_booth' => $favorite,
+            'comment' => $comment
+        ];
+
+        file_put_contents($dataFile, json_encode($records, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+        echo "<script>alert('ขอบคุณสำหรับความคิดเห็นของคุณ!'); history.back();</script>";;
+        exit;
+    } else {
+        echo "invalid";
+        exit;
+    }
+}
+
+?>
